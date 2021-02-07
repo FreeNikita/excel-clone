b90afa7e897148692f942c18099f901a62f4eddb
FreeNikita <freenikita95@gmail.com>
2021-02-08 00:31:42 +0200

    render component

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,6 +1,26 @@
+import {$} from '@core/dom';
+
 export class Excel {
   constructor(selector, option) {
     this.$el = document.querySelector(selector);
     this.componets = option.components || [];
+  }
+
+  getRoot() {
+    const $root = $.create('div', 'excel');
+
+    this.componets.map((Component) => {
+      const $el = $.create('div', Component.className);
+
+      const component = new Component($el);
+      $el.innerHTML = component.toHTML();
+      $root.append($el);
+    });
+
+    return $root;
+  }
+
+  render() {
+    this.$el.append(this.getRoot());
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import '@/scss/style.scss';
 import {Excel} from '@/components/excel';
+import {Header} from '@/components/header';
+import {Toolbar} from '@/components/toolbar';
+import {Formula} from '@/components/formula';
+import {Table} from '@/components/table';
 
 const excel = new Excel('#app', {
   components: [
-
+    Header, Toolbar, Formula, Table,
   ],
 });
 
-console.log('excel', excel);
+excel.render();
+
