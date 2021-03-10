9da98d3d7407d77b17e524e1dba7170d547fd618
FreeNikita <freenikita95@gmail.com>
2021-03-10 18:41:32 +0200

    added logic removeEventListener

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -7,102 +7,22 @@ export class Table extends ExcelComponent {
       return `
         <div class="row">
 
-        <div class="row-info"></div>
+            <div class="row-info"></div>
 
-        <div class="row-data">
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-          <div class="column">
-            A
-          </div>
-          <div class="column">
-            B
-          </div>
-          <div class="column">
-            C
-          </div>
-
-        </div>
-
+            <div class="row-data">
+                <div class="column">
+                    A
+                </div>
+                <div class="column">
+                    B
+                 </div>
+                 <div class="column">
+                    C
+                  </div>
+                <div class="column">
+                   D
+                </div>
+            </div>
         </div>
       `;
     }
